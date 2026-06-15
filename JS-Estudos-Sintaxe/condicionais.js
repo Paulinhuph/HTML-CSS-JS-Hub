9263612627231b7808da9e775cd9ee9f89c22ea2
Paulo Henrique <henriqyepaulo@gmail.com>
2026-06-15 13:23:04 -0300

    feat(condicionais): adicionando novas práticas com condicionais

--- JS-Estudos-Sintaxe/condicionais.js
+++ JS-Estudos-Sintaxe/condicionais.js
@@ -286,3 +286,136 @@ if (formularioValido === true) {
     console.log("Por favor, preencha todos os campos corretamente");
 }
 
+// ─────────────────────────────────────────────────────────
+// Praticando: Condicionais
+// ─────────────────────────────────────────────────────────
+
+// ACESSO LIBERADO?
+// Você está desenvolvendo um sistema de controle de acesso para um evento. Para entrar, a pessoa precisa ter 18 anos ou mais.
+// Crie um programa que, dado o valor da idade, verifique se a pessoa pode ou não entrar no evento. Exiba a mensagem informando se o acesso foi ou não permitido.
+const idade = 21;
+if (idade >= 18) {
+    console.log("Acesso permitido.");
+} else {
+    console.log("Acesso negado.");
+}
+
+// CLASSIFICAÇÂO DE TEMPERATURA.
+const temperatura = 30;
+if (temperatura < 15) {
+    console.log("Frio");
+} else if (temperatura >= 15 && temperatura <= 25) {
+    console.log("Agradável");
+} else {
+    console.log("Quente");
+}
+
+// CLASSIFICAÇÃO DE IDADE PARA ATIVIDADE
+const idadeALuno = 15;
+if (idadeALuno < 12) {
+    console.log("Infantil");
+} else if (idadeALuno >= 12 && idadeAluno < 18) {
+    console.log("Juvenil");
+} else {
+    console.log("Adulto");
+}
+
+// HORÁRIO DE FUNCIONAMENTE DA LOJA
+// Você está desenvolvendo um sistema para controlar o horário de funcionamento de uma loja. Durante a semana, a loja abre das 9h às 18h, mas aos sábados e domingos, ela abre em horário especial, das 10h às 14h.
+// Crie um programa que, dado o dia da semana em formato numérico:
+// 0: Domingo
+// 1: Segunda-feira
+// 2: Terça-feira
+// 3: Quarta-feira
+// 4: Quinta-feira
+// 5: Sexta-feira
+// 6: Sábado
+// Verifique se a loja está aberta e qual o horário de funcionamento.
+// Se for sábado (6) ou domingo (0), a loja estará aberta em horário especial.
+// Nos outros dias, a loja estará aberta no horário normal.
+const diaSemana = 4;
+if (diaSemana >= 1 && diaSemana <= 5) {
+    console.log("A loja está aberta no horário normal: 9h às 18h.");
+} else if (diaSemana === 6 || diaSemana === 0) {
+    console.log("A loja está aberta em horário especial: 10h às 14h.");
+} else {
+    console.log("Informe o dia da Semana correto.");
+}
+
+// VALIDAÇÂO DE LOGIN
+const usuario = "admin";
+const mensagem = (usuario === "admin") ? "Login bem-sucedido!" : "Usuário inválido.";
+console.log(mensagem);
+
+// VERIFICANDO COMPATIBILIDADE DE TIPOS DE FRUTAS
+// Você está desenvolvendo um sistema para verificação de compatibilidade de frutas em um processo de preparo de sucos. O sistema precisa verificar se o tipo de fruta informado é compatível com o tipo esperado, de acordo com uma receita.
+// O suco pode ser feito somente com "laranja" ou "abacaxi".
+// Qualquer outra fruta que não seja "laranja" ou "abacaxi" deve gerar uma mensagem de erro, informando que a fruta não é compatível.
+// Crie um programa que, dado o nome da fruta, verifique se ela é compatível com a receita
+const fruta = "laranja";
+if (fruta !== "laranja" && fruta !== "abacaxi") {
+    console.log("Fruta incompatível para a receita.");
+} else {
+    console.log("Fruta compatível para a receita.")
+}
+
+// CONTROLE DE ACESSO COM MANUTENÇÂO
+// Imagine que você está desenvolvendo um site de compras. A pessoa só pode finalizar a compra se o carrinho não estiver vazio.
+// Crie um programa que, com base na variável carrinhoVazio (booleana), verifique se a compra pode ser finalizada. 
+// Se o carrinho não estiver vazio, mostre: "Compra finalizada com sucesso!". Caso contrário, exiba: "Não é possível finalizar a compra: carrinho vazio."
+const carrinhoVazio = false;
+const compra = (!carrinhoVazio) 
+    ? "Compra Finalizada com Sucesso!" 
+    : "Não é possível finalizar a compra: carrinho vazio.";
+console.log(compra);
+
+
+// VERIFICANDO O STATUS DE PAGAMENTO
+// Você está criando um sistema de verificação de status de pagamento de clientes. Dependendo do status, o sistema deve exibir uma mensagem apropriada:]
+// Se o status for "pendente", exibe: "Pagamento pendente".
+// Se o status for "aprovado", exibe: "Pagamento aprovado".
+// Se o status for "recusado", exibe: "Pagamento recusado".
+// Use o switch/case para criar um programa que faça essa verificação e mostre a mensagem correspondente ao status do pagamento.
+const status = "aprovado";
+switch (status) {
+    case "pendente":
+        console.log("Pagamento pendente");
+        break;
+    case "aprovado":
+        console.log("Pagamento aprovado");
+        break;
+    case "recusado":
+        console.log("Pagamento recusado");
+        break;
+    default:
+        console.log("Status Inválido");
+        break;
+}
+
+//Verificar acesso a nível do jogo
+// Você está criando um sistema para um jogo, onde o personagem tem que passar por duas condições para conseguir avançar para o próximo nível:
+// O personagem precisa ter mais de 50 pontos.
+// O personagem deve ter pelo menos uma vida restante.
+// Crie um programa que, dado os pontos e as vidas restantes, verifique se o personagem pode ou não avançar para o próximo nível.
+const pontos = 55;
+const vida = 0;
+if (pontos > 50 && vida >= 1) {
+    console.log("Próximo nível liberado!")
+} else {
+    console.log("Não pode avançar para o próximo nível.")
+};
+
+//Você está desenvolvendo um sistema que exibe a situação da bateria de um dispositivo com base na porcentagem de carga. O sistema deve classificar a bateria nas seguintes categorias:
+// "Crítica": se a bateria estiver abaixo de 20%
+// "Moderada": se estiver entre 20% e 80%
+// "Cheia": se estiver acima de 80%
+// Utilize o operador ternário para definir e exibir a categoria da bateria com base no valor da variável bateria.
+const bateria = 10;
+ 
+const statusBateria = (bateria < 20) 
+  ? "Crítica" 
+  : (bateria <= 80) 
+    ? "Moderada" 
+    : "Cheia";
+ 
+console.log(statusBateria);
